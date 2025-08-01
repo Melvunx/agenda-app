5cad8a4c3eb7390b57b09db5204493c3e3e811e8
Melvunx <augustine.melv@gmmail.com>
2025-08-01 20:13:08 +0200

    setup all pages

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,12 +1,37 @@
 import { Footer } from "@/src/components/layout/footer";
 import { Header } from "@/src/components/layout/header";
+import { isAuthenticated } from "@/src/server/session";
+import { redirect } from "next/navigation";
+import { Suspense } from "react";
+
+function LoadingScreen() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900"></div>
+    </div>
+  );
+}
+
+async function AuthChecker({ children }: { children: React.ReactNode }) {
+  const authenticated = await isAuthenticated();
+
+  if (!authenticated) {
+    redirect("/signin");
+  }
+
+  return <>{children}</>;
+}
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <section>
-      <Header />
-      {children}
-      <Footer />
-    </section>
+    <Suspense fallback={<LoadingScreen />}>
+      <AuthChecker>
+        <section>
+          <Header />
+          {children}
+          <Footer />
+        </section>
+      </AuthChecker>
+    </Suspense>
   );
 }
